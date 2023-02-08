9b2b2b29830375953b9a38d4cf796e3b67289a9a
Maxime Moreillon <m.moreillon@gmail.com>
2023-02-08 18:15:04 +0900

    pagionation

--- controllers/sources.js
+++ controllers/sources.js
@@ -1,6 +1,6 @@
-const Source = require('../models/source.js')
-const { subscribe_single } = require('../mqtt')
-const { delete_measurement } = require('./points')
+const Source = require("../models/source.js")
+const { subscribe_single } = require("../mqtt")
+const { delete_measurement } = require("./points")
 
 exports.create_source = async (req, res, next) => {
   try {
@@ -11,18 +11,30 @@ exports.create_source = async (req, res, next) => {
   } catch (error) {
     next(error)
   }
-  
 }
-
 
 exports.get_sources = async (req, res, next) => {
   try {
-    const sources = await Source.find({})
-    res.send(sources)
+    const { skip = 0, limit = 50, order = -1, sort = "_id", search } = req.query
+
+    const query = {}
+
+    if (search) {
+      const regex = { $regex: search, $options: "i" }
+      const searchableProperties = ["name", "topic"]
+      query.$or = searchableProperties.map((p) => ({ [p]: regex }))
+    }
+    const total = await Source.countDocuments(query)
+
+    const sources = await Source.find(query)
+      .skip(Number(skip))
+      .sort({ [sort]: order })
+      .limit(Math.max(Number(limit), 0))
+
+    res.send({ skip, limit, order, sort, total, items: sources })
   } catch (error) {
     next(error)
   }
-  
 }
 
 exports.get_source = async (req, res, next) => {
@@ -33,34 +45,32 @@ exports.get_source = async (req, res, next) => {
   } catch (error) {
     next(error)
   }
-  
 }
-
 
 exports.update_source = async (req, res, next) => {
   try {
-    const {_id} = req.params
+    const { _id } = req.params
     const properties = req.body
-    const source = await Source.findOneAndUpdate({_id}, properties, {new: true})
+    const source = await Source.findOneAndUpdate({ _id }, properties, {
+      new: true,
+    })
     console.log(`[MongoDB] Source ${_id} updated`)
     subscribe_single(source)
-    
+
     res.send(source)
-  }
-  catch (error) {
+  } catch (error) {
     next(error)
   }
 }
 
 exports.delete_source = async (req, res, next) => {
   try {
-    const {_id} = req.params
-    const result = await Source.findOneAndDelete({_id})
+    const { _id } = req.params
+    const result = await Source.findOneAndDelete({ _id })
     await delete_measurement({ _id })
     console.log(`[MongoDB] Source ${_id} deleted`)
     res.send(result)
-  }
-  catch (error) {
-    next(error) 
+  } catch (error) {
+    next(error)
   }
 }
